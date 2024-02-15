beb72d3370159edc366588e51cffb5b416059810
Caleb Cushing <xenoterracide@gmail.com>
2024-02-15 17:24:37 -0500

    chore(build): fix spotless without ratcheting

--- src/main/java/com/xenoterracide/gradle/semver/PorcelainGit.java
+++ src/main/java/com/xenoterracide/gradle/semver/PorcelainGit.java
@@ -1,21 +1,11 @@
-/*
- * Copyright 2018 Caleb Cushing
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2018-2024 Caleb Cushing.
 package com.xenoterracide.gradle.semver;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.StreamSupport;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -28,58 +18,50 @@ import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
 import org.eclipse.jgit.lib.Repository;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.StreamSupport;
-
 final class PorcelainGit {
-    private static final String VERSION_PREFIX = "v";
-    private static final String TAG_PREFIX = "refs/tags/";
-    private static final String VERSION_GLOB = VERSION_PREFIX + "[0-9]*.[0-9]*.[0-9]*";
 
-    private final Git git;
+  private static final String VERSION_PREFIX = "v";
+  private static final String TAG_PREFIX = "refs/tags/";
+  private static final String VERSION_GLOB = VERSION_PREFIX + "[0-9]*.[0-9]*.[0-9]*";
 
-    PorcelainGit( Git git ) {
-        this.git = Objects.requireNonNull( git );
+  private final Git git;
+
+  PorcelainGit(Git git) {
+    this.git = Objects.requireNonNull(git);
+  }
+
+  private static <T> T getLast(List<T> list) {
+    return list != null && !list.isEmpty() ? list.get(list.size() - 1) : null;
+  }
+
+  Ref findMostRecentTag() throws IOException {
+    return getLast(getDb().getRefsByPrefix(TAG_PREFIX + VERSION_PREFIX));
+  }
+
+  private Repository getRepo() {
+    return git.getRepository();
+  }
+
+  long countCommits(AnyObjectId a, AnyObjectId b)
+    throws IncorrectObjectTypeException, MissingObjectException, GitAPIException {
+    return StreamSupport.stream(git.log().addRange(a, b).call().spliterator(), true).count();
+  }
+
+  String describe() throws InvalidPatternException, GitAPIException, IOException {
+    String version = git.describe().setMatch(VERSION_GLOB).call();
+    if (version == null) {
+      Ref mostRecentTag = findMostRecentTag();
+      if (mostRecentTag != null) {
+        String vtag = StringUtils.removeStart(mostRecentTag.getName(), TAG_PREFIX);
+        ObjectId head = getRepo().resolve("HEAD");
+        long commitCount = countCommits(mostRecentTag.getPeeledObjectId(), head.toObjectId());
+        version = String.join("-", vtag, String.valueOf(commitCount), 'g' + head.name().substring(0, 7), "SNAPSHOT");
+      }
     }
+    return version;
+  }
 
-    private static <T> T getLast( List<T> list ) {
-        return list != null && !list.isEmpty() ? list.get( list.size() - 1 ) : null;
-    }
-
-    Ref findMostRecentTag() throws IOException {
-        return getLast( getDb().getRefsByPrefix( TAG_PREFIX + VERSION_PREFIX ) );
-    }
-
-    private Repository getRepo() {
-        return git.getRepository();
-    }
-
-    long countCommits( AnyObjectId a, AnyObjectId b )
-            throws IncorrectObjectTypeException, MissingObjectException, GitAPIException {
-        return StreamSupport.stream( git.log().addRange( a, b ).call().spliterator(), true ).count();
-    }
-
-    String describe() throws InvalidPatternException, GitAPIException,
-            IOException {
-        String version = git.describe().setMatch( VERSION_GLOB ).call();
-        if ( version == null ) {
-            Ref mostRecentTag = findMostRecentTag();
-            if ( mostRecentTag != null ) {
-                String vtag = StringUtils.removeStart( mostRecentTag.getName(), TAG_PREFIX );
-                ObjectId head = getRepo().resolve( "HEAD" );
-                long commitCount = countCommits( mostRecentTag.getPeeledObjectId(), head.toObjectId() );
-                version = String.join( "-", vtag, String.valueOf( commitCount ),
-                        'g' + head.name().substring( 0, 7 ), "SNAPSHOT"
-                );
-
-            }
-        }
-        return version;
-    }
-
-    private RefDatabase getDb() {
-        return getRepo().getRefDatabase();
-    }
+  private RefDatabase getDb() {
+    return getRepo().getRefDatabase();
+  }
 }
--- src/main/java/com/xenoterracide/gradle/semver/SemVerPlugin.java
+++ src/main/java/com/xenoterracide/gradle/semver/SemVerPlugin.java
@@ -1,21 +1,9 @@
-/*
- * Copyright 2018 Caleb Cushing
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2018-2024 Caleb Cushing.
 package com.xenoterracide.gradle.semver;
 
+import java.io.IOException;
+import java.util.Optional;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.InvalidPatternException;
@@ -26,30 +14,25 @@ import org.gradle.api.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.Optional;
-
 /**
  * inspired by @see <a href="https://github.com/cinnober/semver-git/">Cinnober's SemVer Git</a>
  */
 public class SemVerPlugin implements Plugin<Project> {
-    private final Logger log = LoggerFactory.getLogger( SemVerPlugin.class );
 
+  private final Logger log = LoggerFactory.getLogger(SemVerPlugin.class);
 
-    @Override
-    public void apply( Project project ) {
-        try {
-            Repository repo = new FileRepositoryBuilder()
-                    .readEnvironment()
-                    .findGitDir( project.getProjectDir() )
-                    .build();
+  @Override
+  public void apply(Project project) {
+    try {
+      Repository repo = new FileRepositoryBuilder().readEnvironment().findGitDir(project.getProjectDir()).build();
 
-            Optional.ofNullable( new PorcelainGit( new Git( repo ) ).describe() )
-                    .map( v -> v.substring( 1 ) )
-                    .map( v -> v.contains( "g" ) ? v + "-SNAPSHOT" : v )
-                    .ifPresent( project::setVersion );
-        } catch ( IOException | InvalidPatternException | GitAPIException e ) {
-            log.error( "", e );
-        }
+      Optional
+        .ofNullable(new PorcelainGit(new Git(repo)).describe())
+        .map(v -> v.substring(1))
+        .map(v -> v.contains("g") ? v + "-SNAPSHOT" : v)
+        .ifPresent(project::setVersion);
+    } catch (IOException | InvalidPatternException | GitAPIException e) {
+      log.error("", e);
     }
+  }
 }
