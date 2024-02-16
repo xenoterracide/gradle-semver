02810c1698525b4b47169d45def2ebee5d8e7dbe
Caleb Cushing <xenoterracide@gmail.com>
2024-02-16 09:25:52 -0500

    chore(build): copy other old code

--- src/test/java/com/xenoterracide/gradle/semver/SemVerPluginTest.java
+++ src/test/java/com/xenoterracide/gradle/semver/SemVerPluginTest.java
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright © 2024 Caleb Cushing.
+// Copyright © 2018-2024 Caleb Cushing.
 package com.xenoterracide.gradle.semver;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,7 +8,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.InitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,41 @@ class SemVerPluginTest {
 
   @TempDir
   File testProjectDir;
+
+  Project project;
+  Git git;
+
+  @BeforeEach
+  void setupProject() throws Exception {
+    project = ProjectBuilder.builder().build();
+
+    var msg1 = "one";
+    var msg2 = "two";
+    var msg3 = "three";
+
+    git = new InitCommand().setDirectory(project.getProjectDir()).call();
+    var one = git.commit().setMessage(msg1).setAllowEmpty(true).call();
+    var two = git.commit().setMessage(msg2).setAllowEmpty(true).call();
+    var three = git.commit().setMessage(msg3).setAllowEmpty(true).call();
+
+    git.tag().setAnnotated(true).setMessage(msg1).setName("v0.1.1").setObjectId(one).call();
+    git.tag().setAnnotated(true).setMessage(msg2).setName("v0.1.2").setObjectId(two).call();
+    git.tag().setAnnotated(true).setMessage(msg3).setName("v0.1.3").setObjectId(three).call();
+  }
+
+  @Test
+  void apply() {
+    project.getPluginManager().apply(SemVerPlugin.class);
+    assertThat(project.getVersion()).isEqualTo("0.1.3");
+  }
+
+  @Test
+  void snapshot() throws Exception {
+    git.commit().setMessage("four").setAllowEmpty(true).call();
+
+    project.getPluginManager().apply(SemVerPlugin.class);
+    assertThat(project.getVersion().toString()).startsWith("0.1.3-1-g").endsWith("-SNAPSHOT");
+  }
 
   @BeforeEach
   @SuppressWarnings("NullAway.Init")
@@ -41,7 +79,7 @@ class SemVerPluginTest {
   }
 
   @Test
-  void t() {
+  void withRunner() {
     var build = GradleRunner
       .create()
       .withDebug(true)
