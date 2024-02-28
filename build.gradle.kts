aa36aa8b0299a0b47ae0cddf272a7fec8f4f1926
Caleb Cushing <xenoterracide@gmail.com>
2024-02-28 15:10:22 -0500

    fix(deps): use simple logging
    * also, properlty expose jgit as api

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,13 @@ dependencyLocking {
 
 dependencies {
   runtimeOnly(platform(libs.slf4j.bom))
-  runtimeOnly(libs.slf4j.nop)
+  runtimeOnly(libs.slf4j.simple)
   compileOnlyApi(libs.jspecify)
+  api(libs.jgit)
+  api(libs.semver)
   implementation(platform(libs.slf4j.bom))
-  implementation(libs.jgit)
   implementation(libs.vavr)
   implementation(libs.guava)
-  api(libs.semver)
   testImplementation(libs.junit.api)
   testImplementation(gradleTestKit())
 }
