428c1b261e5fb24c799f07153cbd3c39ffe85077
Caleb Cushing <xenoterracide@gmail.com>
2024-02-28 14:25:29 -0500

    fix: try excluding the jar from the shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,9 @@ tasks.withType<ShadowJar>().configureEach {
   archiveClassifier.set("")
   relocationPrefix = "com.xenoterracide.gradle.semver"
   isEnableRelocation = true
+  dependencies {
+    exclude { it.moduleName == "slf4j-nop" }
+  }
 }
 
 dependencyAnalysis {
