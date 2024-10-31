b667c251bc552174ae605d5fcafc69346f781ca5
Caleb Cushing <caleb.cushing@gmail.com>
2024-10-31 14:19:03 -0400

    feat: progress

--- module/git/src/main/java/com/xenoterracide/gradle/git/GitSupplierService.java
+++ module/git/src/main/java/com/xenoterracide/gradle/git/GitSupplierService.java
@@ -1,30 +1,35 @@
 // © Copyright 2024 Caleb Cushing
 // SPDX-License-Identifier: Apache-2.0
 
-package com.xenoterracide.gradle.semver.internal;
+package com.xenoterracide.gradle.git;
 
-import com.xenoterracide.gradle.semver.SemverExtension;
-import com.xenoterracide.gradle.semver.internal.AbstractGitService.Params;
-import io.vavr.control.Try;
+import com.xenoterracide.gradle.git.GitSupplierService.Params;
 import java.io.IOException;
 import java.util.Optional;
+import java.util.function.Supplier;
 import javax.inject.Inject;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.util.SystemReader;
+import org.gradle.api.UncheckedIOException;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.services.BuildService;
 import org.gradle.api.services.BuildServiceParameters;
 import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Build Service for Git. Primary goal is to allow for lazy initialization of the Git object and keeping it open for
- * later usage. This Service should not be considered a published API, and may change or be removed in future versions.
+ * Build Service for Git. Primary goal is to allow for lazy initialization of the Git object and
+ * keeping it open for later usage. This Service should not be considered a published API, and may
+ * change or be removed in future versions.
  */
-public abstract class AbstractGitService implements BuildService<Params>, AutoCloseable {
+public abstract class GitSupplierService implements BuildService<Params>, AutoCloseable, Supplier<Optional<Git>> {
   static {
     preventJGitFromCallingExecutables();
   }
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
   private @Nullable Git git;
 
@@ -33,32 +38,7 @@ public abstract class AbstractGitService implements BuildService<Params>, AutoCl
    */
   @Inject
   @SuppressWarnings({ "this-escape", "InjectOnConstructorOfAbstractClass" })
-  public AbstractGitService() {}
-
-  Optional<Git> lazyGit() throws IOException {
-    if (this.git == null) {
-      var projectDir = this.getParameters().getProjectDirectory().get().getAsFile();
-      var gitDir = new FileRepositoryBuilder().readEnvironment().setMustExist(false).findGitDir(projectDir).getGitDir();
-
-      this.git = gitDir != null ? Git.open(gitDir) : null;
-    }
-
-    return Optional.ofNullable(this.git);
-  }
-
-  /**
-   * Create the SemverExtension.
-   *
-   * @return The SemverExtension.
-   */
-  public SemverExtension extension() {
-    return new SemverExtension(() -> Try.of(this::lazyGit).getOrElse(Optional.empty()));
-  }
-
-  @Override
-  public void close() {
-    if (this.git != null) this.git.close();
-  }
+  public GitSupplierService() {}
 
   // preventJGitFromCallingExecutables is copied from
   // https://github.com/diffplug/spotless/blob/224f8f96df3ad42cac81064a0461e6d4ee91dcaf/plugin-gradle/src/main/java/com/diffplug/gradle/spotless/GitRatchetGradle.java#L35
@@ -78,6 +58,28 @@ public abstract class AbstractGitService implements BuildService<Params>, AutoCl
         }
       }
     );
+  }
+
+  @Override
+  public Optional<Git> get() {
+    if (this.git == null) {
+      var projectDir = this.getParameters().getProjectDirectory().get().getAsFile();
+      var gitDir = new FileRepositoryBuilder().readEnvironment().setMustExist(false).findGitDir(projectDir).getGitDir();
+
+      try {
+        this.git = gitDir != null ? Git.open(gitDir) : null;
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
+    return Optional.ofNullable(this.git);
+  }
+
+  @Override
+  public void close() {
+    this.log.info("Closing Git repository");
+    if (this.git != null) this.git.close();
+    this.git = null;
   }
 
   /**
--- module/git/src/main/java/com/xenoterracide/gradle/git/HeadBranchValueSource.java
+++ module/git/src/main/java/com/xenoterracide/gradle/git/HeadBranchValueSource.java
@@ -3,37 +3,80 @@
 
 package com.xenoterracide.gradle.git;
 
+import static com.xenoterracide.gradle.git.GradleTools.finalizeOnRead;
+
 import com.xenoterracide.gradle.git.internal.GitUtils;
-import java.io.IOException;
+import io.vavr.control.Try;
+import java.util.Objects;
 import javax.inject.Inject;
 import org.apache.commons.io.output.ByteArrayOutputStream;
-import org.gradle.api.UncheckedIOException;
-import org.gradle.api.logging.Logger;
+import org.eclipse.jgit.api.Git;
+import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.provider.ValueSource;
 import org.gradle.process.ExecOperations;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Value source for the head branch.
+ * <ol>
+ *   <li>check for remotes</li>
+ *   <li>check if set explicitly</li>
+ *   <li>check git netork</li>
+ * </ol>
+ */
 abstract class HeadBranchValueSource implements ValueSource<String, GitConfigurationExtension> {
 
   private final ExecOperations execOperations;
+  private final GitSupplierService gitS;
+  private final ProviderFactory providerFactory;
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
 
   @Inject
-  protected HeadBranchValueSource(Logger log, ExecOperations execOperations) {
+  protected HeadBranchValueSource(
+    ExecOperations execOperations,
+    GitSupplierService gitS,
+    ProviderFactory providerFactory
+  ) {
     this.execOperations = execOperations;
+    this.gitS = gitS;
+    this.providerFactory = providerFactory;
   }
 
   @Override
   public @Nullable String obtain() {
-    try (var baos = new ByteArrayOutputStream()) {
-      this.execOperations.exec(execSpec -> {
-          execSpec.setExecutable("git");
-          execSpec.args("remote", "show", this.getParameters().getSourceRemote());
-          execSpec.setStandardOutput(baos);
-        }).getExitValue();
+    var hasRemotes =
+      this.gitS.get()
+        .map(Git::remoteList)
+        .map(cmd -> Try.of(cmd::call))
+        .map(t -> t.onFailure(e -> this.log.warn("Failed to get remotes", e)))
+        .map(t -> t.get())
+        .map(remotes -> !remotes.isEmpty())
+        .orElse(false);
 
-      return GitUtils.getHeadBranch(baos);
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
+    if (hasRemotes) {
+      return finalizeOnRead(this.getParameters().getHeadBranch())
+        .orElse(
+          this.providerFactory.provider(() -> {
+              return Try.withResources(ByteArrayOutputStream::new)
+                .of(this::gitRemoteShow)
+                .onFailure(e -> this.log.warn("Failed to get head branch", e))
+                .getOrNull();
+            })
+        )
+        .getOrNull();
     }
+    return null;
+  }
+
+  String gitRemoteShow(ByteArrayOutputStream baos) {
+    this.execOperations.exec(execSpec -> {
+        execSpec.setExecutable("git");
+        execSpec.args("remote", "show", this.getParameters().getSourceRemote());
+        execSpec.setStandardOutput(baos);
+      }).getExitValue();
+
+    return Objects.requireNonNull(GitUtils.getHeadBranch(baos), "unable to locate in " + "output");
   }
 }
