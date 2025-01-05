5584049186b04c39f7c3b8316c5a9763437e367c
Caleb Cushing <caleb.cushing@gmail.com>
2025-01-05 17:22:14 -0500

    chore: more split progress

--- module/git/src/main/java/com/xenoterracide/gradle/git/GitRemoteForGradle.java
+++ module/git/src/main/java/com/xenoterracide/gradle/git/GitRemoteForGradle.java
@@ -4,14 +4,11 @@
 
 package com.xenoterracide.gradle.git;
 
-import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
 import org.gradle.api.provider.Provider;
 import org.jspecify.annotations.Nullable;
 
 public class GitRemoteForGradle {
-
-  private final Logger log = Logging.getLogger(this.getClass());
 
   private final String name;
   private final Provider<@Nullable String> headBranch;
@@ -26,6 +23,13 @@ public class GitRemoteForGradle {
   }
 
   Provider<@Nullable String> getHeadBranch() {
+    if (!this.headBranch.isPresent()) {
+      Logging.getLogger(this.getClass()).warn(
+        "Git remote {} has no HEAD branch; run `git remote set-head {} --auto`",
+        this.name,
+        this.name
+      );
+    }
     return this.headBranch;
   }
 }
