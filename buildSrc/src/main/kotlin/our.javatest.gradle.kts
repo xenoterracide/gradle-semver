4c539363883194b2956673002b78be40d704043b
Caleb Cushing <xenoterracide@gmail.com>
2025-11-24 15:44:16 -0500

    chore/gradle thread safe (#520)

--- buildSrc/src/main/kotlin/our.javatest.gradle.kts
+++ buildSrc/src/main/kotlin/our.javatest.gradle.kts
@@ -54,20 +54,9 @@ val available =
 tasks.withType<Test>().configureEach {
   jvmArgs("-XX:+EnableDynamicAgentLoading")
   useJUnitPlatform()
-  maxParallelForks =
-    Runtime
-      .getRuntime()
-      .availableProcessors()
-      .div(2)
-      .coerceAtLeast(1)
-  systemProperties(
-    "junit.jupiter.execution.parallel.enabled" to "true",
-    "junit.jupiter.execution.parallel.mode.default" to "concurrent",
-    "junit.jupiter.execution.parallel.mode.classes.default" to "concurrent",
-    "junit.jupiter.execution.parallel.config.fixed.parallelism" to "2",
-  )
+  maxParallelForks = Runtime.getRuntime().availableProcessors()
+
   reports {
-    junitXml.required.set(false)
     html.required.set(false)
   }
   testLogging {
