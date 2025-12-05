11b40675fe47baf2af2bb723c7b25fdc3671c981
Caleb Cushing <caleb.cushing@gmail.com>
2025-12-05 08:22:00 -0500

    chore: add nullmarked package-info.java

---
+++ module/git/src/testFixtures/java/com/xenoterracide/gradle/git/fixtures/package-info.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Caleb Cushing
+//
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+@NullMarked
+package com.xenoterracide.gradle.git.fixtures;
+
+import org.jspecify.annotations.NullMarked;
