e9a6596c76643cf498a8a5b00b51b4f7f8bff6e6
Caleb Cushing <caleb.cushing@gmail.com>
2026-03-06 16:07:28 -0500

    test: verify distanceFromTag matches git describe
    Add test to ensure our distance calculation is consistent with
    JGit's DescribeCommand (git describe). This provides a contract
    test that will catch any implementation drift.

--- module/git/src/test/java/com/xenoterracide/gradle/git/DistanceCalculatorTest.java
+++ module/git/src/test/java/com/xenoterracide/gradle/git/DistanceCalculatorTest.java
@@ -7,6 +7,7 @@ package com.xenoterracide.gradle.git;
 import static com.xenoterracide.gradle.git.fixtures.CommitTools.commit;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.base.Splitter;
 import java.io.File;
 import org.eclipse.jgit.api.Git;
 import org.junit.jupiter.api.Test;
@@ -60,6 +61,32 @@ class DistanceCalculatorTest {
       var tagRef = git.getRepository().resolve("v0.1.0");
       assertThat(tagRef).isNotNull();
       assertThat(calculator.distanceBetween(tagRef, commit2)).isEqualTo(2);
+    }
+  }
+
+  @Test
+  void distanceFromTagMatchesGitDescribe() throws Exception {
+    try (var git = Git.init().setDirectory(projectDir).call()) {
+      // Create initial commit and tag
+      commit(git);
+      git.tag().setName("v1.0.0").call();
+
+      // Create 5 more commits
+      for (var i = 0; i < 5; i++) {
+        commit(git);
+      }
+
+      // Get distance from git describe
+      var describeResult = git.describe().setTags(true).setLong(true).call();
+      assertThat(describeResult).isNotNull();
+      // describeResult format: v1.0.0-5-g<sha>
+      var describeDistance = Integer.parseInt(Splitter.on('-').splitToList(describeResult).get(1));
+
+      // Our distance calculation should match git describe
+      var calculator = new DistanceCalculator(() -> git);
+      var ourDistance = calculator.apply("HEAD");
+
+      assertThat(ourDistance).isEqualTo(describeDistance).isEqualTo(5);
     }
   }
 }
