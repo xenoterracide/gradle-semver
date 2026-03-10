97ef4037761ba08033bbb426bc3337c018ff016a
Caleb Cushing <caleb.cushing@gmail.com>
2026-03-10 14:34:23 -0400

    fix(test): remove redundant `MaxRAM` JVM argument from test tasks

--- buildSrc/src/main/kotlin/our.convention.gradle.kts
+++ buildSrc/src/main/kotlin/our.convention.gradle.kts
@@ -116,7 +116,7 @@ testing {
 }
 
 tasks.withType<Test>().configureEach {
-  jvmArgs("-XX:MaxRAM=2g", "-XX:MaxRAMPercentage=80", "-XX:+UseStringDeduplication")
+  jvmArgs("-XX:MaxRAMPercentage=80", "-XX:+UseStringDeduplication")
 }
 
 tasks.check {
