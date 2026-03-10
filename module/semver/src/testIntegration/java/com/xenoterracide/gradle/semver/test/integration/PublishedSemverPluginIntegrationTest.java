83a53f788693680cb260d18fa25a71e9d1696d36
Caleb Cushing <caleb.cushing@gmail.com>
2026-03-10 13:43:30 -0400

    fix(test): update PublishedSemverPluginIntegrationTest to match current build file format

--- module/semver/src/testIntegration/java/com/xenoterracide/gradle/semver/test/integration/PublishedSemverPluginIntegrationTest.java
+++ module/semver/src/testIntegration/java/com/xenoterracide/gradle/semver/test/integration/PublishedSemverPluginIntegrationTest.java
@@ -98,8 +98,11 @@ class PublishedSemverPluginIntegrationTest {
   private static void patchBootstrapBuild(Path sourceCopy) throws IOException {
     var buildFile = sourceCopy.resolve("build.gradle.kts");
     var buildScript = Files.readString(buildFile).replace(
-      "version = if (providers.environmentVariable(\"IS_PUBLISHING\").isPresent) " +
-        "calculateProjectVersion() else \"0.0.0\"",
+      "version =\n" +
+        "  providers\n" +
+        "    .environmentVariable(\"IS_PUBLISHING\")\n" +
+        "    .flatMap { semver.provider }\n" +
+        "    .getOrElse(Semver.ZERO)",
       "version = \"" + VERSION + "\""
     );
     Files.writeString(buildFile, buildScript);
